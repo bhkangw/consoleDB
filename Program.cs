db48f940dec8ee6053042811e9fcada461d8239c
Brian Kang <bhkangw@gmail.com>
2017-11-30 17:55:48 -0800

    Added NewUser and ShowUser methods

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using DbConnection;
+using System.Collections.Generic;
 
 namespace consoleDB
 {
@@ -7,11 +8,36 @@ namespace consoleDB
     {
         static void Main(string[] args)
         {
-            var users = DbConnector.Query("SELECT * FROM users");
-            foreach (var user in users)
+            ShowUsers();
+            // NewUser();
+        }
+        static void ShowUsers()
+        {
+            List<Dictionary<string, object>> myQuery = new List<Dictionary<string, object>>();
+            myQuery = DbConnector.Query("SELECT * FROM users");
+            foreach(Dictionary<string, object> user in myQuery)
             {
-                System.Console.WriteLine(user);
+                System.Console.WriteLine(user["FirstName"] + " " + user["LastName"] + ": " + user["FavoriteNumber"]);
             }
         }
+
+        static void NewUser()
+        {
+            System.Console.WriteLine("What is your first name?");
+            string fname = Console.ReadLine();
+            System.Console.WriteLine("What is your last name?");
+            string lname = Console.ReadLine();
+            System.Console.WriteLine("What is your favorite number?");
+            string number = Console.ReadLine();
+            DbConnector.Execute($"INSERT INTO users (FirstName, LastName, FavoriteNumber) VALUES ('{fname}','{lname}',{number})");
+        }
+
+
+        // static void FindUser(int id)
+        // {
+        //     Dictionary<string, object> user = new Dictionary<string, object>();
+        //     user = DbConnector.Query($"SELECT * FROM users WHERE id = {id}");
+            
+        // }
     }
 }
