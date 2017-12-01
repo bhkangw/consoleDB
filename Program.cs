808171f727c01941305c17865a1a7cc3320d2006
Brian Kang <bhkangw@gmail.com>
2017-11-30 16:35:52 -0800

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DbConnection;
+
+namespace consoleDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var users = DbConnector.Query("SELECT * FROM users");
+            foreach (var user in users)
+            {
+                System.Console.WriteLine(user);
+            }
+        }
+    }
+}
